6e38afaf4a7362c61b59ae200128cab5e1d688b2
WarBuggy <hovanbuu@gmail.com>
2026-02-05 13:25:47 -0800

    * Remove checking if actions.list exist because creation is guarantee by EngineManager in C#

--- Mods/Core/Scripts/action.lua
+++ Mods/Core/Scripts/action.lua
@@ -1,20 +1,13 @@
 local function createActionList(modId, defName, type)
+    
     if type ~= "action" then
         return
     end
-
+    
     local coreModId = "Core"
     local rootPath = "actions.list"
-    
     -- Try to get the action LedgerMap from Core
     local ledger, exists = GameData:TryGetFrom(coreModId, rootPath)
-
-    if not exists or ledger == nil then
-        -- Create a new LedgerMap if it doesn't exist
-        ledger = LedgerMap.Create()
-        GameData.SetTo(coreModId, rootPath, ledger)
-    end
-
     -- Set the definition name with modId as value, actorId = Core
     LedgerMap.Set(ledger, defName, modId)
 end
